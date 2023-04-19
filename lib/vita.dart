317111051ea138537414a2a884430ba8eca0866c
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-04-19 04:55:51 +0330

    2023.04.19 - 3 (separation of app.dart from main.dart)

--- lib/vita.dart
+++ lib/vita.dart
@@ -7,6 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
 import 'main.dart';
 
 typedef Vita = SplayTreeMap<String, Luna>;
